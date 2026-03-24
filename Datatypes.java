358d86f180cd05d140c9d15dec9c658f1491a2af
sonnyyga123-dotcom <sonnyyga123@gmail.com>
2026-03-24 06:58:17 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,48 @@
+class Datatypes 
+{ 
+public static void main(String[] args) 
+{ 
+byte myByte1,myByte2; 
+myByte1 = 120; 
+myByte2 = -48; 
+System.out.println("Byte1: " +myByte1); 
+System.out.println("Byte2: " +myByte2); 
+ 
+short myShort = 6000; 
+System.out.println("\nShort:" +myShort); 
+int myInteger1, myInteger2; 
+ 
+myInteger1 = -7000; 
+myInteger2 = 9000; 
+System.out.println("\nInteger1:"+myInteger1); 
+System.out.println("Integer2:"+myInteger2); 
+ 
+long myLong1, myLong2; 
+myLong1 = 100000000L; 
+myLong2 = 200L; 
+System.out.println("\nLong1: " +myLong1); 
+System.out.println("Long2: " +myLong2);
+  
+float myFloat1,myFloat2;  
+myFloat1=1000.666f; 
+myFloat2=110.77f;  
+System.out.println("\nFloat1: "+myFloat1); 
+System.out.println("Float2: "+myFloat2);
+  
+double myDouble1, myDouble2;  
+myDouble1 = 48976.8987; 
+myDouble2 = 29513.7812d; 
+System.out.println("\nDouble1: " +myDouble1); 
+System.out.println("Double2: " +myDouble2); 
+
+boolean myBool = true; 
+if(myBool == true) 
+System.out.println("\nI am using a Boolean data type"); 
+System.out.println(myBool);
+ 
+char myChar1 = 'A'; 
+char myChar2 = 66; 
+System.out.println("\nmyChar1: " +myChar1); 
+System.out.println("myChar2: " +myChar2);  
+} 
+}
